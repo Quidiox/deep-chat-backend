71aa822efb53f651860b6ce21c5ae228472c29db
Quidiox <33523865+Quidiox@users.noreply.github.com>
2018-12-10 18:56:39 +0200

    CORS_ORIGIN env variable

--- src/utils/config.js
+++ src/utils/config.js
@@ -3,5 +3,6 @@ if (process.env.NODE_ENV !== 'production') require('dotenv').config()
 module.exports = {
   port: process.env.PORT,
   mongoURI: process.env.MONGODB_URI,
-  secret: process.env.SECRET
+  secret: process.env.SECRET,
+  origin: process.env.CORS_ORIGIN
 }
