a7f8ce4f7774b10be53a36a40b82a2cf968981fb
Quidiox <33523865+Quidiox@users.noreply.github.com>
2019-03-29 17:34:46 +0200

    user edit and create

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -3,7 +3,7 @@ const jwt = require('jsonwebtoken')
 const userRouter = require('express').Router()
 const User = require('../models/User')
 const config = require('../utils/config')
-const { check, validationResult } = require('express-validator/check')
+const { check, oneOf, validationResult } = require('express-validator/check')
 
 const validations = [
   check('name')
@@ -20,6 +20,27 @@ const validations = [
     .withMessage('Username must contain only alphanumeric characters.'),
   check('password')
     .isLength({ min: 3, max: 30 })
+    .withMessage('Password must be between 3-30 characters long.')
+]
+
+const validationsMaybePass = [
+  check('name')
+    .custom(name => {
+      return name.match(/^[a-zA-Z\s]+$/)
+    })
+    .withMessage('Name must contain only alphabetic characters.')
+    .isLength({ min: 3, max: 30 })
+    .withMessage('Name must be between 3-30 characters long.'),
+  check('username')
+    .isLength({ min: 3, max: 30 })
+    .withMessage('Username must be between 3-30 characters long.')
+    .isAlphanumeric()
+    .withMessage('Username must contain only alphanumeric characters.'),
+  check('password')
+    .custom(password => {
+      if (password) return password.match(/^.{3,30}$/)
+      return true
+    })
     .withMessage('Password must be between 3-30 characters long.')
 ]
 
@@ -84,13 +105,53 @@ userRouter.post('/create', validations, async (req, res) => {
   }
 })
 
-userRouter.delete('/remove/:id', async (req, res) => {
+userRouter.put('/edit', validationsMaybePass, async (req, res) => {
   try {
-    await User.findByIdAndRemove(req.params.id)
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+      return res.status(422).json({ errors: errors.array() })
+    }
+    const { username, name, password, id } = req.body
+    const user = await User.findById(id)
+    user.username = username
+    user.name = name
+    if (password) {
+      const saltRounds = 10
+      const passwordHash = await bcrypt.hash(password, saltRounds)
+      user.passwordHash = passwordHash
+    }
+    console.log(user)
+    const savedUser = await user.save()
+    const userForToken = {
+      username: savedUser.username,
+      id: savedUser.id
+    }
+    const token = jwt.sign(userForToken, config.secret)
+    res.cookie('token', token, {
+      httpOnly: true,
+      maxAge: 3600000
+    })
+    res.json({
+      username: savedUser.username,
+      name: savedUser.name,
+      id: savedUser.id
+    })
+  } catch (error) {
+    if (error.code === 11000) {
+      res.status(409).json({ error: 'username exists' })
+    } else {
+      res.status(500).json({ error: 'editing user failed' })
+    }
+  }
+})
+
+userRouter.delete('/delete', async (req, res) => {
+  try {
+    await User.findByIdAndRemove(req.body.id)
     res.status(204).end()
   } catch (error) {
     // console.log(error)
-    res.status(500).json({ error: 'failed horribly' })
+    res.status(500).json({ error: 'deleting user failed' })
   }
 })
 
