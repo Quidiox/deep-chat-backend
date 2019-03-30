4f3e27106d5b80e8ff875500dfbf043e56ac1226
Quidiox <33523865+Quidiox@users.noreply.github.com>
2019-03-30 14:41:53 +0200

    User model fixed to get mongoose 5.4.20 to work

--- src/models/User.js
+++ src/models/User.js
@@ -1,31 +1,40 @@
 const mongoose = require('mongoose')
 
-const userSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    index: true,
-    unique: true,
-    required: true,
-    dropDups: true
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      index: true,
+      unique: true,
+      required: true,
+      dropDups: true
+    },
+    name: { type: String, required: true },
+    passwordHash: { type: String, required: true }
   },
-  name: { type: String, required: true },
-  passwordHash: { type: String, required: true }
-})
-
-if (!userSchema.options.toObject) userSchema.options.toObject = {}
-
-userSchema.options.toObject.transform = function(doc, ret) {
-  return {
-    id: ret._id,
-    username: ret.username,
-    name: ret.name,
-    passwordHash: ret.passwordHash
+  {
+    toObject: {
+      transform: function(doc, ret) {
+        return {
+          id: ret._id,
+          username: ret.username,
+          name: ret.name,
+          passwordHash: ret.passwordHash
+        }
+      }
+    },
+    toJSON: {
+      transform: function(doc, ret) {
+        return {
+          id: ret._id,
+          username: ret.username,
+          name: ret.name,
+          passwordHash: ret.passwordHash
+        }
+      }
+    }
   }
-}
-
-userSchema.post('init', function(doc) {
-  doc.toObject()
-})
+)
 
 const User = mongoose.model('User', userSchema)
 
